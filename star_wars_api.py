16547568282f3f2761a936b44a846b249b79b63d
Hareesh Sikakollu <hareeshsikakollu@gmail.com>
2022-03-31 10:48:00 +0530

    Mysql Connection

--- star_wars_api.py
+++
@@ -1,5 +0,0 @@
-import requests,json
-data = requests.get('http://swapi.dev/api/people').json()
-json_object = json.dumps(data,indent=6)
-with open('star_wars_data.txt', 'w') as f:
-    f.write(json_object)
